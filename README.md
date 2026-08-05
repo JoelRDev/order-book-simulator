9f454f647bc894309131748a4b3f3af330fcdb14
JoelRDev <jvrautavaara@gmail.com>
2026-08-05 15:10:39 +1000

    feat: establish diff stream

---
+++ README.md
@@ -0,0 +1,5 @@
+# Order Book Simulator
+This project is intended to collect order book data and use it for backtesting strategies with L2 order book data
+
+## Limitations
+- Order book only updates every 100ms (changes are aggregated)
--- data/data_collector.py
+++ data/data_collector.py
@@ -1,10 +1,21 @@
 import websockets
 import asyncio
 import requests
+import json
 
-ws_stream = 'wss://fstream.binance.com/public/ws/!bookTicker'
-api_url = 'https://fapi.binance.com/fapi/v1/depth?symbol=BTCUSDT&limit=1000'
+symbol = 'btcusdt'
 
-res = requests.get(api_url).json()
-bids = res['bids']
-print(bids)
+def snapshot(symbol):
+    symbol.upper()
+    url = f'https://fapi.binance.com/fapi/v1/depth?symbol={symbol}&limit=1000'
+    res = requests.get(url).json()
+
+async def diff_stream(symbol):
+    url = f'wss://fstream.binance.com/ws/{symbol}@depth'
+    async with websockets.connect(url) as ws:
+        async for message in ws:
+            event = json.loads(message)
+            yield event
+
+async def main():
+    pass
