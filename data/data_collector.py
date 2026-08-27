7cf8bf1478fdc963877291e66a4c804b42cab527
JoelRDev <jvrautavaara@gmail.com>
2026-08-27 17:17:35 +1000

    fix: straddling order

--- data/data_collector.py
+++ data/data_collector.py
@@ -9,28 +9,21 @@ def snapshot(symbol):
     url = f'https://fapi.binance.com/fapi/v1/depth?symbol={symbol.upper()}&limit=1000'
     return requests.get(url).json()
 
-async def diff_stream(symbol):
-    url = f'wss://fstream.binance.com/ws/{symbol}@depth'
-    async with websockets.connect(url) as ws:
-        async for message in ws:
-            event = json.loads(message)
-            yield event
-
 def straddle(snapshot, ws_event):
-    if ws_event['U'] <= snapshot['lastUpdateId'] <= ws_event['u']:
-        return True
+    return ws_event['U'] <= snapshot['lastUpdateId'] <= ws_event['u']
 
 async def main():
-    initial = snapshot(symbol)
-    try:
+    url = f'wss://fstream.binance.com/ws/{symbol}@depth'
+    async with websockets.connect(url) as ws:
+        initial = await asyncio.to_thread(snapshot, symbol)
         async with asyncio.timeout(5):
-            async for event in diff_stream(symbol):
+            async for message in ws:
+                event = json.loads(message)
+                if event['u'] < initial['lastUpdateId']:
+                    continue
                 if straddle(initial, event):
-                    break
-            else:
-                return
-    except TimeoutError:
-        return
+                    return initial, event
+                raise RuntimeError('Straddling failed')
 
 if __name__ == "__main__":
     asyncio.run(main())
