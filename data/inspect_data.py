cd970bb1ffe95de0e8b9d141e0ad11cbac858c77
JoelRDev <jvrautavaara@gmail.com>
2026-08-28 11:48:00 +1000

    feat: filter parquet data before converting to pd.df

--- data/inspect_data.py
+++ data/inspect_data.py
@@ -1,6 +1,11 @@
 import pandas as pd
+import pyarrow.dataset as ds
 
-df = pd.read_parquet('market_data')
+dataset = ds.data('market_data', format='parquet')
+table = dataset.to_table(
+    filter=ds.field('event_time_ms') >= 1_700_000_000_000
+)
+df = table.to_pandas()
 print(df.info())
 print(df.head())
 print(df.describe())
