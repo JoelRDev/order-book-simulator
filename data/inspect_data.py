24f393f6a20ca9747103671b24780d0a9ceb40bb
JoelRDev <jvrautavaara@gmail.com>
2026-08-28 12:01:18 +1000

    fix: data -> dataset typo

--- data/inspect_data.py
+++ data/inspect_data.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import pyarrow.dataset as ds
 
-dataset = ds.data('market_data', format='parquet')
+dataset = ds.dataset('market_data', format='parquet')
 table = dataset.to_table(
     filter=ds.field('event_time_ms') >= 1_700_000_000_000
 )
